bdaca1ba2af3cc32ff87a60f2f64743cfb7d5419
James Butcher <jamesbutcher167@gmail.com>
2023-05-20 20:24:00 +0100

    Add index

--- src/jbutcher5_github_io/core.clj
+++ src/jbutcher5_github_io/core.clj
@@ -1,8 +1,24 @@
 (ns jbutcher5-github-io.core
   (:gen-class)
-  (:use hiccup.core))
+  (:require [hiccup.page :refer [html5 include-css]]
+            [clojure.java.io :refer [writer]]))
+
+(def index
+  (html5
+   [:head
+    [:title "James Portfolio"]
+    (include-css "styles.css")]
+   [:body
+    [:h1 "Lorem Ipsum"]]))
+
+(def pages [["index" index]])
+
+(defn write-pages [pages]
+  (let [x (first pages) xs (rest pages)]
+    (with-open [wtr (writer (str "page/" (first x) ".html"))]
+      (.write wtr (second x)))
+    (when (> (count xs) 0) (write-pages xs))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println (html [:p])))
+  [& _]
+  (write-pages pages))
