592962a0bcb8f6e9ec402b802218b3512fcb7dd2
James Butcher <jamesbutcher167@gmail.com>
2023-05-21 15:55:02 +0100

    Update project details

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject jbutcher5-github-io "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject jbutcher5-github-io "0.1.0"
+  :description "A static personal website"
+  :url "https://jbutcher5.github.io"
+  :license {:name "MIT"
+            :url "https://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [hiccup "1.0.5"]]
   :main ^:skip-aot jbutcher5-github-io.core
